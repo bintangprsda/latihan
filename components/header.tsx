16dca14b65168b1063b5fa56049e21421a69484b
User <user@Users-MacBook-Pro.local>
2024-04-09 17:11:32 +0700

    Pesan commit Anda di sini

---
+++ components/header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white py-4 px-6 md:px-12">
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-2xl font-bold">Bintangprsda</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
